f4e3b94a91de3d6cfa9287188e9341e4216aef55
Romain Deville <code@romaindeville.fr>
2025-09-23 00:22:32 +0200

    ✨ Add file to deploy and manage OpenBao identity groups
    Add base modules files to deploy OpenBao groups.
    
    Since a single group should be manage by the module, every attache policies,
    identity entity or ideneity groups are attache with the `exclusive` parameter
    set to true.
    
    If another team deploy the same group with other attachment, this will lead to
    infrastructure drift.
    
    This beahaviour is expective as I consider if this happens, it means something
    went wrong, either someone gain access to the vault or a another team is making
    update to the group deployed by this module without using dedicate
    implementation.

---
+++ variables.tf
@@ -0,0 +1,112 @@
+# Module Specific variables
+# -----------------------------------------------------------------------------
+variable "namespace_path" {
+  type        = string
+  description = <<-EOM
+  Namespace where to deploy the identity if not the namespace set in the
+  provider.
+  EOM
+
+  default  = null
+  nullable = true
+}
+
+variable "name" {
+  type        = string
+  description = "String to set the name of the group"
+}
+
+variable "type" {
+  type        = string
+  description = <<-EOM
+  Type of the group, internal or external (Forces new resource).
+  EOM
+
+  default  = "internal"
+  nullable = false
+}
+
+variable "metadata" {
+  type        = map(string)
+  description = "A Map of additional metadata to associate with the group."
+
+  default  = {}
+  nullable = false
+}
+
+variable "skip_member_group_verification" {
+  type        = bool
+  description = <<-EOM
+  Boolean to skip evaluation of the existence of subgroups.
+  Useful when deploying subgroups with parent groups.
+  EOM
+
+  default  = false
+  nullable = false
+}
+
+variable "member_group_ids" {
+  type        = set(string)
+  description = <<-EOM
+  A list of Group IDs to be assigned as group members. Not allowed on external
+  groups.
+  EOM
+
+  default  = []
+  nullable = false
+}
+
+variable "member_group_names" {
+  type        = set(string)
+  description = <<-EOM
+  A list of Group Name to be assigned as group members. Not allowed on external
+  groups.
+  EOM
+
+  default  = []
+  nullable = false
+}
+
+variable "skip_member_entity_verification" {
+  type        = bool
+  description = <<-EOM
+  Boolean to skip evaluation of the existence of subgroups.
+  Useful when deploying subgroups with parent groups.
+  EOM
+
+  default  = false
+  nullable = false
+}
+
+
+
+variable "member_entity_ids" {
+  type        = set(string)
+  description = <<-EOM
+  A list of Entity IDs to be assigned as group members. Not allowed on external
+  groups.
+  EOM
+
+  default  = []
+  nullable = false
+}
+
+variable "member_entity_names" {
+  type        = set(string)
+  description = <<-EOM
+  A list of Entity Name to be assigned as group members. Not allowed on external
+  groups.
+  EOM
+
+  default  = []
+  nullable = false
+}
+
+variable "policies" {
+  type        = list(string)
+  description = <<-EOM
+  List of policies IDs or Names to attach to the group.
+  EOM
+
+  default = []
+}
